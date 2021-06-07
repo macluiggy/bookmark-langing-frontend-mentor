3d78f80bea79581348e36c8ebdebd6616b227683
Luiggy Macias <ferrinluiggy@gmail.com>
2021-06-07 14:12:31 -0500

    adding the burger

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
 import './css/header.css'
-import React, {Component} from 'react';
+import React, { Component } from 'react';
+
+import { $, $$, p } from './functions.js'
 
 class Header extends Component {
-	
 
-	render() {
-		return <header>
+    state = {
+        clase: 'burger'
+    }
+
+    changeClass = () => {
+        this.setState({
+            clase: this.state.clase === 'burger' ? 'change' : 'burger'
+        })
+    }
+
+    render() {
+        return <header>
         <nav>
+            <figure></figure>
+            <div className={this.state.clase} onClick={this.changeClass}>
+                <div class="bar1"></div>
+                <div class="bar2"></div>
+                <div class="bar3"></div>
+            </div>
             <ul>
                 <li>Features</li>
                 <li>Pricing</li>
@@ -20,7 +37,7 @@ class Header extends Component {
         <button>Get it on Chrome</button>
         <button>Get it on Firefox</button>
     </header>
-	}
+    }
 }
 
 export default Header;
